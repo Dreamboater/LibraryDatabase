a1794de501d5839b0705621db6e00aff70fd0755
vandervaart <a.vandervaart@woodland.on.ca>
2025-03-05 10:13:20 -0500

    2nd

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("aaaaaaahhhhh")
+print("Welcome to the library database software program.")
+
+file_list = []
+file = open('database.txt', 'r')
+for line in file:
+    file_list.append(line)
+file.close()
+
+while True:
+    response = input("What would you like to do?\n Add \n Remove \n Display all \n Exit \n")
+
+    if response == "list":
+        print(file_list)
+    if response == "Display all":
+        print("\n")
+        for line in file_list:
+            print(line, end='')
+        print("\n")
+
+    if response == "Add":
+        file_list.append(input("Please enter the name of the book:")+"\n")
+
+    if response == "Remove":
+        file_list.remove(input("Please enter the name of the book:"))
+
+    if response == "Exit":
+        file = open('database.txt', 'a')
+        for item in file_list:
+            file.write(item)
+        break
+
+
+
+
+
+
+
